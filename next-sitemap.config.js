a1bb68675ac7ad641adddfb79258c71a8bfc6f28
Jared Gentry <jared.c.gentry@gmail.com>
2025-03-18 13:27:47 -0400

    adds sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://getgistr.com", // Your website URL
+  generateRobotsTxt: true, // Generate a robots.txt file
+  exclude: [], // No need to exclude pages here
+};
